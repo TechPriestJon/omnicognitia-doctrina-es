dc4667ba1d8ca61c0f625a2411454813a2310e2a
unknown <13856887+TechPriestJon@users.noreply.github.com>
2024-12-23 16:32:44 -0800

    phase 1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'omnicognitia-doctrina-es',
+  webDir: 'dist'
+};
+
+export default config;
